6f084b3f0b4136cb3dc0b800ea1cbbf00b438eb8
philuu12 <philuu12@gmail.com>
2022-03-11 19:50:18 -0800

    Add files via upload

---
+++ utils/make_request.py
@@ -0,0 +1,31 @@
+import requests
+import json
+
+
+def make_http_request(method: object, url: object, header: object = None, data: object = None) -> object:
+    # response = make_request.make_request("POST", url=api + "products", data=json.dumps(product), header=header)
+    if method == "GET":
+        return requests.get(url)
+    if method == "DELETE":
+        return requests.delete(url)
+    if method == "POST":
+        return requests.post(url=url, headers=header, data=json.dumps(data))
+    if method == "PUT":
+        return requests.put(url=url, headers=header, data=json.dumps(data))
+    if method == "PATCH":
+        return requests.patch(url=url, headers=header, data=json.dumps(data))
+
+
+def make_request(method, url, header=None, data=None):
+    if method == "GET":
+        return requests.get(url)
+    if method == "DELETE":
+        return requests.get(url)
+    if method == "POST":
+        return requests.post(url=url, headers=header, data=data)
+    if method == "PUT":
+        return requests.put(url=url, headers=header, data=data)
+    if method == "PATCH":
+        return requests.patch(url=url, headers=header, data=data)
+
+
