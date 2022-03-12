1e71e3353142016073ee4bb15b52fb076c88e6ff
philuu12 <philuu12@gmail.com>
2022-03-11 19:48:41 -0800

    Delete utils directory

--- utils/utils/gen_endpoint.py
+++
@@ -1,9 +0,0 @@
-# Generate endpoint
-# Path parameters are found within the path of the endpoint before the query string (?)
-
-
-def generate_endpoint(front_resource, id="", back_resource=""):
-    if id:
-        return front_resource + "/" + str(id) + "/" + back_resource
-    else:
-        return front_resource + "/"
