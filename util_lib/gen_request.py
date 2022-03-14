726dfd881f45dc6d0cf3042d8a1c96630db64400
philuu12 <philuu12@gmail.com>
2022-03-13 20:15:46 -0700

    Delete util_lib directory

--- util_lib/gen_request.py
+++
@@ -1,7 +0,0 @@
-# import requests
-
-
-def gen_http_request(base_url, end_point):
-    return base_url + end_point
-
-
--- util_lib/verify_test_framework.py
+++
@@ -1,8 +0,0 @@
-def assert_status_code(operation, expected_code, actual_code):
-    if expected_code != actual_code:
-        assert False, "Expected operation \"{}\" to return status code {}, instead of status code {}".format(operation, expected_code, actual_code)
-
-
-def assert_data(field, expected_data, actual_data):
-    if expected_data != actual_data:
-        assert False, "Expected data for \"{}\" to be {}, instead got {}".format(field, expected_data, actual_data)
